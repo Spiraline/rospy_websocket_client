0c54f21eed9cc1bf9fb2a967530b6311d88c5fb4
Spiraline <spiraline@rideflux.com>
2024-01-29 15:21:29 +0900

    Implement sub pub client

--- rospy_websocket_client/ros_ws_client.py
+++ rospy_websocket_client/ros_ws_client.py
@@ -1,10 +1,9 @@
 import websocket
-import rel
 import json
 import yaml
 import rospy
 import time
-from uuid import uuid4
+from rospy_message_converter import message_converter
 
 class ws_client():
   def __init__(self, name='', ip='127.0.0.1', port=9090):
@@ -16,10 +15,14 @@ class ws_client():
     self._address = "ws://" + self._ip + ":" + str(self._port)
     self._is_connected = False
 
+    self._advertise_dict = {}
     self._sub_dict = {}
-    self._pub_dict = {}
 
     self.connect()
+  
+  def __del__(self):
+    if self._ws is not None:
+      self._ws.close()
     
   # Connect to rosbridge websocket
   def connect(self):
@@ -30,33 +33,101 @@ class ws_client():
         )
         print("%s:%s\t|\tConnected to server" % (self._ip, self._port))
         self._is_connected = True
+        self._ws.settimeout(1)
       except:
-        print("%s:%s\t|\tError connecting to server. Waiting for 5 seconds.." % (ip, port))
+        print("%s:%s\t|\tError connecting to server. Waiting for 5 seconds.." % (self._ip, self._port))
         self._connect_flag = False
         time.sleep(5)
 
-  
-  def _callback(self, msg, name):
-    # Converting ROS message to a dictionary thru YAML
-    ros_message_as_dict = yaml.safe_load(msg.__str__())
-
-    json_dict = {
-      'name': name,
-      'type': self._sub_dict[name]["type"],
-      'msg': ros_message_as_dict
-    }
-    json_msg = json.dumps(json_dict)
-    
+  def recv(self):
     try:
-      self._ws.send(json_msg)
+      json_raw = self._ws.recv()
     except:
-      print("%s:%s\t|\Fail to send msg %s" % (self._ip, self._port, name))
+      print("%s:%s\t|\tFail to recv msg" % (self._ip, self._port))
+      return None
+    
+    json_msg = json.loads(json_raw)
+
+    if json_msg['op'] == 'publish':
+      msg_dict = json_msg['msg']
+      topic_name = json_msg['topic']
+      topic_type = self._sub_dict[topic_name]['msg']._type
+      ros_msg = message_converter.convert_dictionary_to_ros_message(topic_type, msg_dict)
+
+      return ros_msg
+
+    return None
+  
+  def _advertise(self, topic_name, topic_type):
+    """
+    Advertise a topic with it's type in 'package/Message' format.
+    :param str topic_name: ROS topic name.
+    :param str topic_type: ROS topic type, e.g. std_msgs/String.
+    """
+    self._advertise_dict[topic_name] = {'topic_type': topic_type}
+    advertise_msg = {"op": "advertise",
+                      "topic": topic_name,
+                      "type": topic_type
+                      }
+
+    try:
+      self._ws.send(json.dumps(advertise_msg))
+    except:
+      print("%s:%s\t|\Fail to advertise msg %s" % (self._ip, self._port, topic_name))
       self._is_connected = False
       self.connect()
-  
-  def advertise(self, topic_name, topic_type):
-    type_str = topic_type().__class__.__name__
-    self._sub_dict[topic_name] = {'type': type_str,
-                                  'subscriber': rospy.Subscriber(topic_name, topic_type, self._callback, topic_name)}
 
-    print("%s:%s\t|\tAdvertise %s with type %s" % (self._ip, self._port, topic_name, type_str))
+    print("%s:%s\t|\tAdvertise %s with type %s" % (self._ip, self._port, topic_name, topic_type))
+    
+  def _subscribe(self, topic_name, msgs_data, rate = 0.0, queue_size = 0):
+    _rate = 0.0
+
+    if rate > 0.0:
+        _rate = 1000.0 / rate
+
+    pub_msg = {
+      'op': 'subscribe',
+      'topic': topic_name,
+      'msgs_data': msgs_data._type,
+      'throttle_rate': int(_rate),
+      'queue_length': int(queue_size)
+    }
+
+    # send to server
+    json_msg = json.dumps(pub_msg)
+    self._ws.send(json_msg)
+    print("%s:%s\t|\tSubscribe to: %s \ttype: %s \trate: %s \tqueue_length: %s" % (self._ip, self._port, topic_name, msgs_data._type, rate, queue_size))
+  
+  def subscribe(self, topic_name, msgs_data, rate = 0, queue_size = 0):
+    pub = rospy.Publisher(topic_name, msgs_data.__class__, queue_size=queue_size)
+    self._sub_dict[topic_name] = {
+      'msg': msgs_data,
+      'publisher': pub,
+      'rate': rate,
+      'queue_size': queue_size
+    }
+
+    self._subscribe(topic_name, msgs_data, rate, queue_size)
+
+  def publish(self, topic_name, ros_message):
+    # If not advertised, advertise topic
+    if topic_name not in self._advertise_dict:
+      topic_type = ros_message._type
+      self._advertise(topic_name, topic_type)
+
+    ros_message_as_dict = yaml.safe_load(ros_message.__str__())
+    
+    msg = {
+      'op': 'publish',
+      'topic': topic_name,
+      'msg': ros_message_as_dict
+    }
+    json_msg = json.dumps(msg)
+
+    try:
+      self._ws.send(json_msg)
+      print("%s:%s\t|\tPublish msg %s" % (self._ip, self._port, topic_name))
+    except:
+      print("%s:%s\t|\tFail to publish msg %s" % (self._ip, self._port, topic_name))
+      self._is_connected = False
+      self.connect()
--- sub_client.py
+++ sub_client.py
@@ -3,11 +3,12 @@ from rospy_websocket_client import ros_ws_client as ros_ws
 from std_msgs.msg import String
 
 if __name__ == "__main__":
-  ws_pub_client = ros_ws.ws_client('pub_client')
-  
-  # Test ROS msg publisher
+  ws_sub_client = ros_ws.ws_client('sub_client')
+  ws_sub_client.subscribe('/test_string', String(), 10, 1)
+
   rate = rospy.Rate(10)
 
   while not rospy.is_shutdown():
-    ws_pub_client.publish('/test_string', String())
+    msg = ws_sub_client.recv()
+    print(msg.data)
     rate.sleep()
